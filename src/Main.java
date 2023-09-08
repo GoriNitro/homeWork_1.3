62e404b3929fa5866bc892dba2f77eb4aca3e69d
GoriNitro <zazasihamid081@gmail.com>
2023-09-08 14:56:09 +0300

    up2

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,40 @@
 import java.util.Arrays;
+
 //дз#1
 public class Main {
-        public static void main(String[] args) {
-            double[] masiv1= {16.0, 3.5, -2.5, 7.0, -4.5, -9.5, 10.0, -5.5, 4.0, -1.5, 9.0, -3.5, 7.5, 8.5, 2.0};
-            System.out.println(Arrays.toString(masiv1));
-            double average = 0.0;
-            int count = 0;
-            for (int i = 0; i < masiv1.length; i++) {
-                if (masiv1[i] < 0) {
-                    for (int j = i + 1; j < masiv1.length; j++) {
-                        if (masiv1[j] > 0) {
-                            average = average + masiv1[j];
-                            count++;
-                        }
+    public static void main(String[] args) {
+        double[] arr = {16.0, 3.5, -2.5, 7.0, -4.5, -9.5, 10.0, -5.5, 4.0, -1.5, 9.0, -3.5, 7.5, 8.5, 2.0};
+        System.out.println(Arrays.toString(arr));
+        double sum = 0.0;
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < 0) {
+                for (int j = i + 1; j < arr.length; j++) {
+                    if (arr[j] > 0) {
+                        sum += arr[j];
+                        count++;
                     }
-                    break;
+                }
+                break;
+            }
+        }
+        System.out.println("Average = " + sum / count);
+        System.out.println("--------------------------------------------");
+//        16.0, 3.5, -2.5, 7.0, -4.5, -9.5, 10.0, -5.5, 4.0, -1.5, 9.0, -3.5, 7.5, 8.5, 2.0
+        for (int i = 0; i < arr.length; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
                 }
             }
-            System.out.println(average);
-            System.out.println(count);
-            System.out.println(average / count);
+            double a = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = a;
+            System.out.println(Arrays.toString(arr));
         }
     }
+}
+
+
 
